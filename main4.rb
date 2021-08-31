c8ef8129b4beec0f5cf80cba4a72d0ad70f651ed
Roberto Daza Castro <serviciosrobertodaza@gmail.com>
2021-08-30 20:59:35 -0500

    Leyendo lineas de archivo de forma eficiente.

--- main4.rb
+++ main4.rb
@@ -1,15 +1,30 @@
-contenido = File.read("clientes2.csv")
-lineas = contenido.split("\n")
+# contenido = File.read("clientes2.csv")
+# lineas = contenido.split("\n")
 
-pp lineas
-mejores_clientes = []
-lineas.each do |line|
-  columnas = line.split(",")
-	if columnas.last.to_i > 9200
-		mejores_clientes.push({nombre: columnas[0], email: columnas[1], puntos: columnas[2].to_i})
+# pp lineas
+# mejores_clientes = []
+# lineas.each do |line|
+#   columnas = line.split(",")
+# 	if columnas.last.to_i > 9200
+# 		mejores_clientes.push({nombre: columnas[0], email: columnas[1], puntos: columnas[2].to_i})
+# 	end
+# end
+# puts "="*20
+# puts "Mejores Clientes"
+# puts "="*20
+# pp mejores_clientes
+
+def best_clients
+	filename = "clientes.csv"
+	clients = []
+	File.foreach(filename) do |line|
+		column = line.split(",")
+			if column.last.to_i > 9200
+				clients.push({nombre: column[0], email: column[1], puntos: column[2].to_i})
+			end
 	end
+	clients
 end
-puts "="*20
-puts "Mejores Clientes"
-puts "="*20
-pp mejores_clientes
+
+puts "Estos son mis mejores clientes"
+pp best_clients
