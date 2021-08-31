c0a35c99d772450c9565900a82672156cb987863
Roberto Daza Castro <serviciosrobertodaza@gmail.com>
2021-08-30 21:10:56 -0500

    Leyendo archivo usando gema csv.

--- main4.rb
+++ main4.rb
@@ -1,3 +1,4 @@
+require 'csv'
 # contenido = File.read("clientes2.csv")
 # lineas = contenido.split("\n")
 
@@ -14,17 +15,24 @@
 # puts "="*20
 # pp mejores_clientes
 
-def best_clients
-	filename = "clientes.csv"
-	clients = []
-	File.foreach(filename) do |line|
-		column = line.split(",")
-			if column.last.to_i > 9200
-				clients.push({nombre: column[0], email: column[1], puntos: column[2].to_i})
-			end
-	end
-	clients
+# def best_clients
+# 	filename = "clientes.csv"
+# 	clients = []
+# 	File.foreach(filename) do |line|
+# 		column = line.split(",")
+# 			if column.last.to_i > 9200
+# 				clients.push({nombre: column[0], email: column[1], puntos: column[2].to_i})
+# 			end
+# 	end
+# 	clients
+# end
+
+# puts "Estos son mis mejores clientes"
+# pp best_clients
+filename = "clientes.csv"
+clientes = []
+CSV.foreach(filename, headers: true, header_converters: :symbol)do |row|
+	clientes.push(row.to_h)
 end
 
-puts "Estos son mis mejores clientes"
-pp best_clients
+pp clientes
